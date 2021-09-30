d7c0b74823ff90d8bb7a5d87862318ad86fa9a84
austinkrz <austinkrzciok@novuslabs.com>
2021-09-30 12:29:10 -0700

    Adds JNI_2 - project demonstrating overloaded C++ methods being called by Java via the JNI

---
+++ JNI_2/native_c/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/native_c.cpp 
+
+OBJS += \
+./src/native_c.o 
+
+CPP_DEPS += \
+./src/native_c.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/lib/jvm/java-11-openjdk-amd64/include/linux -I/usr/lib/jvm/java-11-openjdk-amd64/include -O2 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
