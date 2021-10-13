4f520be57415e74b40fd370d25a395440d621d1e
Austin Krzciok <austinkrzciok@novuslabs.com>
2021-10-12 17:35:35 -0700

    Working with Object Arrays

--- JNI_4/native_c/Debug/src/subdir.mk
+++ JNI_4/native_c/Debug/src/subdir.mk
@@ -5,18 +5,21 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
 ../src/native_c.cpp \
+../src/object_arrays.cpp \
 ../src/override.cpp \
 ../src/primitive_arrays.cpp \
 ../src/primitives.cpp 
 
 OBJS += \
 ./src/native_c.o \
+./src/object_arrays.o \
 ./src/override.o \
 ./src/primitive_arrays.o \
 ./src/primitives.o 
 
 CPP_DEPS += \
 ./src/native_c.d \
+./src/object_arrays.d \
 ./src/override.d \
 ./src/primitive_arrays.d \
 ./src/primitives.d 
